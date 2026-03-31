bfe8406c2693a19cc32373c6d349ffa6f5ababad
mathewdillian02 <mathewdillian02@gmail.com>
2026-03-31 22:53:55 +0000

    hi

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os                          
 from flask import Flask, request, abort
 from linebot.v3 import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
@@ -12,7 +12,9 @@ app = Flask(__name__)
 # Replace with your actual credentials from the LINE Developers Console
 configuration = Configuration(access_token='awXQa5SDbERn7bO27T+FXFMZ5kXxs+Mxr7aY2v/zuD6jAlWjZLB9AMCMAtwbfVvD4gEc9VXrrrMN8/VA+0nzy3dc+ez/RL7V40N8croZXcpKO4DPzJrq8iJ5y4CWqrqjT5w9qVGpazIeBNZcSr5bXAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('6c3c4cdb4b604aef2dbdda4d52074003')
-
+@app.route("/")
+def home():
+    return "bot is running"
 @app.route("/callback", methods=['POST'])
 def callback():
     # Get X-Line-Signature header value
@@ -40,9 +42,11 @@ def handle_message(event):
                 messages=[TextMessage(text=event.message.text)]
             )
         )
+import os
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 from linebot.v3.webhooks import MessageEvent, TextMessageContent
 CONF = Configuration(access_token='awXQa5SDbERn7bO27T+FXFMZ5kXxs+Mxr7aY2v/zuD6jAlWjZLB9AMCMAtwbfVvD4gEc9VXrrrMN8/VA+0nzy3dc+ez/RL7V40N8croZXcpKO4DPzJrq8iJ5y4CWqrqjT5w9qVGpazIeBNZcSr5bXAdB04t89/1O/w1cDnyilFU=')
